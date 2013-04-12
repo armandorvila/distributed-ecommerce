7781809d91783de405975a8e0b57073392725f19
Armando <armando.ramirez.vila@gmail.com>
2013-04-12 10:46:43 +0300

    Update TcpArmandoShopService.cs
    English grammar

--- ArmandoShop-MiddleTier/Services/TcpArmandoShopService.cs
+++ ArmandoShop-MiddleTier/Services/TcpArmandoShopService.cs
@@ -5,7 +5,7 @@ namespace ArmandoShop.Services
     public class TcpArmandoShopService:ArmandoShopService
     {
         /*This is a marker class (instead of a marker interface :) )
-      for say to expose ArmandoShopService as Tcp service.
+         to say to expose ArmandoShopService as Tcp service.
       */
     }
 }
